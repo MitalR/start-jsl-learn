2eaf70eeeb946206ef7269617f318141d9e70cbf
MitalR <mittal12714@gmail.com>
2024-09-19 08:09:13 +0000

    conversionjs

---
+++ basics_1/3_conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = 33
+// console.log(typeof score);      
+// console.log(typeof(score));
+
+// let valueInumber = Number(score)
+// console.log(typeof valueInumber);
+// console.log(valueInumber)
+
+// const fruits = {
+    
+// }
+
+// let start1 = "hello"
+// let start2 = "mital"
+
+// let start3 = start1 + start2
+// console.log(start3);
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+//  let gameCounter = 2 + 2
+//  gameCounter ++;
+//  console.log(gameCounter);
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+// console.log("2" > 1 );
+// console.log("02" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
---
+++ basics_1/variabls.js
@@ -0,0 +1,7 @@
+const accountId = 1445524
+let accountEmail = "mital123@gamil.com"
+var accountPassword = "85258"
+accountCity = "Bangaluru"
+console.table([accountId, accountEmail, accountPassword, accountCity ])
+
+  
